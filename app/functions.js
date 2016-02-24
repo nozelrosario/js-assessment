3e253d854a079344c00790fe30637de1f38fb0f3
Nozel Rosario <nozelrosario@gmail.com>
2016-02-24 12:52:55 +0530

    functions done

--- app/functions.js
+++ app/functions.js
@@ -14,35 +14,60 @@ exports.functionsAnswers = {
     },
 
     makeClosures: function (arr, fn) {
-		return arr.map(function(item){ 
-			var sqr_num = fn(item);
-			var sqr = function(){ 
-						return sqr_num; 
-					};
-			return sqr; 
-		});
+        return arr.map(function (item) {
+            var sqr_num = fn(item);
+            var sqr = function () {
+                return sqr_num;
+            };
+            return sqr;
+        });
     },
 
     partial: function (fn, str1, str2) {
-		var op=str1+ ', ' +str2;
-		return function(str3) {
-			return op+str3;
-		};
+        return function (str3) {
+            var par_fn = function () { return fn(str1, str2, str3); };
+            return par_fn();
+        };
     },
 
     useArguments: function () {
-
+        var sum = 0;
+        for (var i = 0; i < arguments.length; i++) {
+            sum += arguments[i];
+        }
+        return sum;
     },
 
     callIt: function (fn) {
-
+        var args = [];
+        for (var i = 1; i < arguments.length; i++) args.push(arguments[i]);
+        return fn.apply(this, args);
     },
 
     partialUsingArguments: function (fn) {
-
+        var args = [];
+        for (var i = 1; i < arguments.length; i++) args.push(arguments[i]);
+        return function () {
+            for (var i = 0; i < arguments.length; i++) args.push(arguments[i]);;
+            var par_fn = function () {
+                return fn.apply(this, args);
+            };
+            return par_fn();
+        };
     },
 
     curryIt: function (fn) {
-
+        return function (arg1) {
+            var par_fn1 = function (arg2) {
+                var par_fn2 = function (arg3) {
+                    var par_fn3 = function () {
+                        return fn.apply(this, [arg1, arg2, arg3]);
+                    }
+                    return par_fn3();
+                };
+                return par_fn2;
+            };
+            return par_fn1;
+        };
     }
 };
